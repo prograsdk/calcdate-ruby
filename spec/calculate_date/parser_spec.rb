231b37839d2646f825df49e99ba7b74dbfb5c9c5
Jesper Sand Nielsen <jesper.sand.nielsen@gmail.com>
2020-05-17 23:37:07 +0200

    add more parser specs

--- spec/calculate_date/parser_spec.rb
+++ spec/calculate_date/parser_spec.rb
@@ -26,58 +26,166 @@ RSpec.describe CalculateDate::Parser do
     end
 
     context 'when parsing number PLUS number' do
-      it 'token should be PLUS' do
-        lexer = CalculateDate::Lexer.new("1 + 2")
-        parser = CalculateDate::Parser.new(lexer)
+      context 'BinaryOperator' do
+        it 'token should be PLUS' do
+          lexer = CalculateDate::Lexer.new("1 + 2")
+          parser = CalculateDate::Parser.new(lexer)
 
-        node = parser.parse
+          node = parser.parse
 
-        expect(node.token.type).to eq(CalculateDate::Token::PLUS)
+          expect(node.token.type).to eq(CalculateDate::Token::PLUS)
+        end
+
+        it 'should parse number PLUS number' do
+          lexer = CalculateDate::Lexer.new("1 + 2")
+          parser = CalculateDate::Parser.new(lexer)
+
+          node = parser.parse
+
+          expect(node).to be_a(CalculateDate::AST::BinaryOperator)
+        end
+
+        context 'Left Expr - Number' do
+          it 'should parse left_expr number' do
+            lexer = CalculateDate::Lexer.new("1 + 2")
+            parser = CalculateDate::Parser.new(lexer)
+
+            node = parser.parse
+
+            expect(node.left_expr.value).to eq(1)
+          end
+
+          it 'left_expr should be Number' do
+            lexer = CalculateDate::Lexer.new("1 + 2")
+            parser = CalculateDate::Parser.new(lexer)
+
+            node = parser.parse
+
+            expect(node.left_expr).to be_a(CalculateDate::AST::Number)
+          end
+        end
+
+        context 'Right Expr - Number' do
+          it 'should parse right_expr number' do
+            lexer = CalculateDate::Lexer.new("1 + 2")
+            parser = CalculateDate::Parser.new(lexer)
+
+            node = parser.parse
+
+            expect(node.right_expr.value).to eq(2)
+          end
+
+          it 'right_expr should be Number' do
+            lexer = CalculateDate::Lexer.new("1 + 2")
+            parser = CalculateDate::Parser.new(lexer)
+
+            node = parser.parse
+
+            expect(node.right_expr).to be_a(CalculateDate::AST::Number)
+          end
+        end
       end
+    end
 
-      it 'should parse number PLUS number' do
-        lexer = CalculateDate::Lexer.new("1 + 2")
-        parser = CalculateDate::Parser.new(lexer)
+    context 'when parsing number PLUS number MINUS number' do
+      context 'BinaryOperator' do
+        it 'token should be MINUS' do
+          lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+          parser = CalculateDate::Parser.new(lexer)
 
-        node = parser.parse
+          node = parser.parse
 
-        expect(node).to be_a(CalculateDate::AST::BinaryOperator)
-      end
+          expect(node.token.type).to eq(CalculateDate::Token::MINUS)
+        end
 
-      it 'should parse left_expr number' do
-        lexer = CalculateDate::Lexer.new("1 + 2")
-        parser = CalculateDate::Parser.new(lexer)
+        it 'should parse number MINUS number' do
+          lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+          parser = CalculateDate::Parser.new(lexer)
 
-        node = parser.parse
+          node = parser.parse
 
-        expect(node.left_expr.value).to eq(1)
-      end
+          expect(node).to be_a(CalculateDate::AST::BinaryOperator)
+        end
 
-      it 'left_expr should be Number' do
-        lexer = CalculateDate::Lexer.new("1 + 2")
-        parser = CalculateDate::Parser.new(lexer)
+        context 'Left Expr - BinaryOperator' do
+          it 'token should be PLUS' do
+            lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+            parser = CalculateDate::Parser.new(lexer)
 
-        node = parser.parse
+            node = parser.parse
 
-        expect(node.left_expr).to be_a(CalculateDate::AST::Number)
-      end
+            expect(node.left_expr.token.type).to eq(CalculateDate::Token::PLUS)
+          end
 
-      it 'should parse right_expr number' do
-        lexer = CalculateDate::Lexer.new("1 + 2")
-        parser = CalculateDate::Parser.new(lexer)
+          it 'should parse number PLUS number' do
+            lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+            parser = CalculateDate::Parser.new(lexer)
 
-        node = parser.parse
+            node = parser.parse
 
-        expect(node.right_expr.value).to eq(2)
-      end
+            expect(node.left_expr).to be_a(CalculateDate::AST::BinaryOperator)
+          end
 
-      it 'right_expr should be Number' do
-        lexer = CalculateDate::Lexer.new("1 + 2")
-        parser = CalculateDate::Parser.new(lexer)
+          context 'Left Expr - Number' do
+            it 'should parse left_expr number' do
+              lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+              parser = CalculateDate::Parser.new(lexer)
 
-        node = parser.parse
+              node = parser.parse
 
-        expect(node.right_expr).to be_a(CalculateDate::AST::Number)
+              expect(node.left_expr.left_expr.value).to eq(1)
+            end
+
+            it 'left_expr should be Number' do
+              lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+              parser = CalculateDate::Parser.new(lexer)
+
+              node = parser.parse
+
+              expect(node.left_expr.left_expr).to be_a(CalculateDate::AST::Number)
+            end
+          end
+
+          context 'Right Expr - BinaryOperator' do
+            it 'should parse right_expr binary operator' do
+              lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+              parser = CalculateDate::Parser.new(lexer)
+
+              node = parser.parse
+
+              expect(node.left_expr.right_expr.value).to eq(2)
+            end
+
+            it 'right_expr should be Number' do
+              lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+              parser = CalculateDate::Parser.new(lexer)
+
+              node = parser.parse
+
+              expect(node.left_expr.right_expr).to be_a(CalculateDate::AST::Number)
+            end
+          end
+        end
+
+        context 'Right Expr - Number' do
+          it 'should parse right_expr number' do
+            lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+            parser = CalculateDate::Parser.new(lexer)
+
+            node = parser.parse
+
+            expect(node.right_expr.value).to eq(3)
+          end
+
+          it 'right_expr should be Number' do
+            lexer = CalculateDate::Lexer.new("1 + 2 - 3")
+            parser = CalculateDate::Parser.new(lexer)
+
+            node = parser.parse
+
+            expect(node.right_expr).to be_a(CalculateDate::AST::Number)
+          end
+        end
       end
     end
   end
